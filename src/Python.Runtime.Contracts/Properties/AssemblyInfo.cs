65d1eda707b0c5b948a55f7ec7d7384d032e65b1
dse <dmitriy.se@gmail.com>
2016-12-29 02:31:21 +0400

    Cross compiled for NetStandard 1.5 and Net 4.6, + empty projects added.

---
+++ src/Python.Runtime.Contracts/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Python.Contracts")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3a75b887-30f2-4f42-b384-47f8d1e1db65")]
